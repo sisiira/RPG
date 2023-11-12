662f79f607b015d02ec7aca5ab85fe146447fdaf
sisiira <150608452+sisiira@users.noreply.github.com>
2023-11-13 00:37:04 +0100

    Add files via upload

---
+++ RPPPPP/RPPPPP/ForetAventure.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPPPPP
+{
+    public class ForetAventure : Aventure
+    {
+        public ForetAventure() : base("Aventure en Forêt", "Vous entrez dans une forêt sombre et mystérieuse.")
+        {
+        }
+
+        public override void CommencerAventure(Personnage personnage)
+        {
+            Console.WriteLine("Vous commencez l'aventure : " + Titre);
+            Console.WriteLine(Description);
+
+            Console.WriteLine("Vous trouvez un coffre caché. Que voulez-vous faire?");
+            Console.WriteLine("1- Ouvrir le coffre");
+            Console.WriteLine("2- Continuer votre chemin");
+
+            string choix = Console.ReadLine();
+
+            switch (choix)
+            {
+                case "1":
+                    Console.WriteLine("Vous avez trouvé un trésor !");
+                    personnage.gagnerExperience(10);
+                    Console.WriteLine("Vous gagnez 10 points d'expérience.");
+                    break;
+                case "2":
+                    Console.WriteLine("Vous continuez votre chemin.");
+                    break;
+            }
+        }
+    }
+
+
+}
+
